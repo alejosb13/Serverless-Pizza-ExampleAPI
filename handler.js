70aea8fcdc8bfc438d82463f60d0ea5d5f1ce9eb
alejosb13 <alejosb13@gmail.com>
2022-01-17 21:05:37 -0300

    SQS conexion

--- handler.js
+++ handler.js
@@ -1,15 +1,43 @@
 'use strict';
 const uuidv1 = require('uuid');
-module.exports.hacerPedido = async (event,context,callback) => {
+const AWS    = require('aws-sdk');
+
+let sqs          = new AWS.SQS({region: process.env.REGION});
+const QUEUE_URL  = process.env.PENDING_ORDER_QUEUE;
+
+module.exports.hacerPedido = (event,context,callback) => {
   const orderId = uuidv1.v1()
   
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `El Id del pedido es ${orderId}`,
-    })
-
+  const params = {
+    MessageBody: JSON.stringify({ orderId: orderId}),
+    QueueUrl : QUEUE_URL
   }
-  
-  callback(null,response)
+
+  sqs.sendMessage(params, (err,data)=>{
+    if(err){
+      sendResponse(500,err,callback)
+    }else{
+      const message = {
+        orderId: orderId,
+        messageId: data.MessageId
+      }
+      sendResponse(200, message,callback)
+    }
+    
+  })
 };
+
+module.exports.prepararPedido = (event,context,callback) => {
+  console.log("prepararPedido");
+  console.log(event);
+  callback()
+};
+
+function sendResponse(statusCode, message, callback){
+  const response = {
+    statusCode,
+    body: JSON.stringify(message)
+  }
+  callback(null,response)
+  
+}
